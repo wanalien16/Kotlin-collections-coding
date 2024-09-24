f1fe13f14d6483a6b1369a1a032bc920269b3998
anilsai <anilsai.damayapally16@gmail.com>
2024-09-24 16:18:03 +0530

    implementing different ways of let scoped function

--- src/main/kotlin/LetScopedFunction.kt
+++ src/main/kotlin/LetScopedFunction.kt
@@ -4,7 +4,7 @@ package org.example
 class Person() {
     var name: String = "Anil"
     var phone_num: String = "999999999"
-    var address: String = "xyz, ngngngnngngng"
+    var address: String? = null
     fun displayInfo() {
         print("name = $name, \n contact is $phone_num, Address is $address")
     }
@@ -14,4 +14,14 @@ fun main(){
 return@let "the name of the person is ${it.name}"
     }
     println(person)
+
+    val phoneNumber = Person().let {
+        "the phone number of the person is ${it.phone_num}"
+    }
+    println(phoneNumber)
+
+    val address = Person().address?.let {
+        return@let "address is ${it}"
+    }
+    println(address)
 }
