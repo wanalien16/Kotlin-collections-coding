6d895785ab6d89af5e2b689ccbc62321b82911a4
anilsai <anilsai.damayapally16@gmail.com>
2024-09-24 20:16:35 +0530

    replace all elements in a immutable list of type data class.

---
+++ src/main/kotlin/ReplaceAll.kt
@@ -0,0 +1,21 @@
+package org.example
+
+data class Player(val id: Int,
+    val matchesPlayed: Int,
+    val totalRuns: Int,
+    val name: String,
+    val team: String)
+
+fun main(){
+  val playerList = listOf<Player>(
+      Player(1, 200, 35000, "Goutham", "ABC"),
+      Player(2,250,42000,"nagu","DC"),
+      Player(3,100,30000,"umesh","fgh")
+  )
+
+    val updatedPlayers = replaceTeam(playerList, "SRH")
+    updatedPlayers.forEach { player-> println(player) }
+}
+fun replaceTeam(list: List<Player>, newTeam: String) : List<Player> {
+     return list.map { player -> player.copy(team = newTeam)  }
+}
